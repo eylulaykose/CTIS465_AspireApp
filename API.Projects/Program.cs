1dfb68a2488b2667b1c5fecac8631c43c4962490
Eylül Ayköse <eylul.aykose@ug.bilkent.edu.tr>
2025-11-20 00:37:39 +0300

    fixing bugs for successful POST operation

--- API.Projects/Program.cs
+++ API.Projects/Program.cs
@@ -3,23 +3,37 @@ using APP.Projects.Features.Tags;
 using CORE.APP.Features;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+// ======================
+// Add services
+// ======================
+
+// 1) DbContext registration (SQLite)
 var connectionString = builder.Configuration.GetConnectionString(nameof(ProjectsDb));
 builder.Services.AddDbContext<DbContext, ProjectsDb>(options =>
     options.UseSqlite(connectionString));
 
+// 2) MediatR registration – scan assemblies
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(TagCreateHandler).Assembly));
 
+// 3) Controllers + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ProjectsDb>();
+    db.Database.EnsureCreated();
+}
 
 app.MapDefaultEndpoints();
 
@@ -30,9 +44,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- APP.Projects/Domain/Tag.cs
+++ APP.Projects/Domain/Tag.cs
@@ -1,13 +1,16 @@
-﻿using CORE.APP.Domain;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APP.Projects.Domain
 {
-    public class Tag : Entity
+    public class Tag
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
         [Required]
         [StringLength(150)]
         public string Name { get; set; }
-
     }
 }
